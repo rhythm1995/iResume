e7599bdc722e1cae7c7ceaa2f613bd4c32feb94d
Bugzhang <rhythm1995@users.noreply.github.com>
2017-07-28 23:52:06 +0800

    项目初始化

---
+++ src/views/index.vue
@@ -0,0 +1,25 @@
+<style scoped type="less">
+
+</style>
+<template>
+    <div class="index">
+        <Sidebar></Sidebar>
+    </div>
+</template>
+<script>
+    import Sidebar from '../components/sidebar.vue';
+    export default {
+        name:'index',
+        components: {
+            Sidebar
+        },
+        methods: {
+            handleStart() {
+                this.$Modal.info({
+                    title: 'Bravo',
+                    content: 'Now, enjoy the convenience of iView.'
+                });
+            }
+        }
+    };
+</script>
