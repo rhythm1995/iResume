f99a3e0a1b6ffc178ba63d53a5fb600736fd2831
bugzhang <flyhighdream@qq.com>
2017-07-31 21:37:41 +0800

    Ajax数据并绑定

--- src/components/sidebar.vue
+++ src/components/sidebar.vue
@@ -1,31 +1,54 @@
 <template>
     <div class="sidebar">
         <div class="sidebar_top">
-            <h1>{{username}}</h1>
-            <img src="../images/avt.png" alt=""/>
+            <h1></h1>
+            <img :src="base.image" alt="" />
         </div>
         <div class="details">
             <h3>手机：</h3>
-            <p>{{phone}}</p>
+            <p>{{base.phone}}</p>
             <h3>邮箱：</h3>
-            <p><a :href="email">{{email}}</a></p>
+            <p>
+                <a :href="'Mailto:'+'{{base.email}}'">{{base.email}}</a>
+            </p>
             <h3>微信：</h3>
-            <h3>{{wechat}}</h3>
+            <p>{{base.wechat}}</p>
+            <h3>个人博客：</h3>
+            <p>
+                <a :href="base.blog">{{base.blog}}</a>
+            </p>
+            <h3>GitHub：</h3>
+            <p>
+                <a :href="base.github">{{base.github}}</a>
+            </p>
         </div>
         <div class="clearfix"></div>
     </div>
 </template>
 
 <script>
-    export default {
-        name: 'sidebar',
-        data () {
-            return {
+import jsonp from 'jsonp';
 
-            };
-        },
-        methods: {}
-    };
+export default {
+    name: 'sidebar',
+    data() {
+        return {
+            base: {}
+        };
+    },
+    methods: {
+
+    },
+    created() {
+        jsonp('http://localhost:3000/data', null, (err, data) => {
+            if (err) {
+                console.error(err.message);
+            } else {
+                this.base = data[0].base;
+            }
+        });
+    },
+};
 </script>
 
 <style scoped>
